8a1b2f4763383e761aea8a15ca55fa1405c13297
berenguer <berenguer@users.noreply.github.com>
2014-12-16 23:34:05 +0100

    appel au CardParser sur une carte de test

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,6 +3,8 @@ package controller;
 public class Main {
 
     public static void main(String[] args) {
+        CardParser.parse("mini-db/Arachnus Spinner.card");
+        
         System.out.println("Alloy me honey");
 
     }
