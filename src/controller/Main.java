45f5a9377d1bbc201aacee242c87f5522014e7fb
berenguer <berenguer@users.noreply.github.com>
2014-12-18 00:01:25 +0100

    Nouvel appel/test du parser pour extraire les editions

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -3,7 +3,9 @@ package controller;
 public class Main {
 
     public static void main(String[] args) {
-        CardParser.parse("mini-db/Arachnus Spinner.card");
+        
+        CardParser.parseEditions("mini-db");
+        //CardParser.parse("mini-db/Arachnus Spinner.card");
         
         System.out.println("Alloy me honey");
 
